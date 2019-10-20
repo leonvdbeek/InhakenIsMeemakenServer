02a0860e39d051c461ad725ce4cc0a45003a7f4d
Léon <leon.vandebeek@xs4all.nl>
2019-10-20 22:33:46 +0200

    Deleted cra

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql')
-
 const app = express();
 var PORT = process.env.PORT || 5000;
 
 const getAllActivities = 'SELECT * FROM attendance';
 
-const c = mysql.createConnection({
+var db_config = {
     host: 'eu-cdbr-west-02.cleardb.net',
     user: 'b3a70cfbfeb1b7',
     password: '428bdc3e',
     database: 'heroku_f4f131df6a793e8'
-})
-c.connect(err => {
-    if(err) {
-        return err;
+  };
+  
+var c = mysql.createConnection(db_config);
+c.connect(er => {
+    if(er) {
+        handleDisconnect();
     }
 });
+
+function handleDisconnect() {
+    c = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+
+    c.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    c.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
 app.use(cors());
 
 app.get('/', (req, res) => {

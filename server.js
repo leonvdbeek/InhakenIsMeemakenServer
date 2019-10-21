d397bd9566107a580a7adada96dfabe9e5f6b003
Léon <leon.vandebeek@xs4all.nl>
2019-10-21 18:18:09 +0200

    Fixed crashing

--- server.js
+++ server.js
@@ -6,40 +6,33 @@ var PORT = process.env.PORT || 5000;
 
 const getAllActivities = 'SELECT * FROM attendance';
 
-var db_config = {
+var dbConfig = {
     host: 'eu-cdbr-west-02.cleardb.net',
     user: 'b3a70cfbfeb1b7',
     password: '428bdc3e',
     database: 'heroku_f4f131df6a793e8'
   };
   
-var c = mysql.createConnection(db_config);
-c.connect(er => {
-    if(er) {
-        handleDisconnect();
-    }
-});
-
+var connection;
 function handleDisconnect() {
-    c = mysql.createConnection(db_config); // Recreate the connection, since
-                                                    // the old one cannot be reused.
-
-    c.connect(function(err) {              // The server is either down
-        if(err) {                                     // or restarting (takes a while sometimes).
+    connection = mysql.createConnection(dbConfig);  // Recreate the connection, since the old one cannot be reused.
+    connection.connect( function onConnect(err) {   // The server is either down
+        if (err) {                                  // or restarting (takes a while sometimes).
             console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
-    c.on('error', function(err) {
+            setTimeout(handleDisconnect, 10000);    // We introduce a delay before attempting to reconnect,
+        }                                           // to avoid a hot loop, and to allow our node script to
+    });                                             // process asynchronous requests in the meantime.
+                                                    // If you're also serving http, display a 503 error.
+    connection.on('error', function onError(err) {
         console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        if (err.code == 'PROTOCOL_CONNECTION_LOST') {   // Connection to the MySQL server is usually
             handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
+        } else {                                        // connnection idle timeout (the wait_timeout
             throw err;                                  // server variable configures this)
         }
     });
 }
+handleDisconnect();
 
 app.use(cors());
 
